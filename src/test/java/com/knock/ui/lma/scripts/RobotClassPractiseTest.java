bfe5126d1cb01b475467db93ebc51731836a21af
sarayu-sree <sarayu.sree1599@gmail.com>
2026-03-20 18:53:08 +0530

    update changes

--- src/test/java/com/knock/ui/lma/scripts/RobotClassPractiseTest.java
+++ src/test/java/com/knock/ui/lma/scripts/RobotClassPractiseTest.java
@@ -1,18 +1,18 @@
-package com.knock.ui.lma.scripts;
-
-import com.knock.ui.base.BaseTest;
-import com.knock.ui.lma.pages.PasswordCheck;
-import org.testng.annotations.Test;
-
-import java.awt.*;
-
-public class RobotClassPractiseTest extends BaseTest {
-    
-    @Test
-    public void testRobotFunctionality() throws AWTException {
-        PasswordCheck passwordCheck = new PasswordCheck(driver);
-        passwordCheck.openURL("https://ccnp-app.knocktest.com/");
-        passwordCheck.login();
-        passwordCheck.googlePasswordManager();
-    }
-}
+//package com.knock.ui.lma.scripts;
+//
+//import com.knock.ui.base.BaseTest;
+//import com.knock.ui.lma.pages.PasswordCheck;
+//import org.testng.annotations.Test;
+//
+//import java.awt.*;
+//
+//public class RobotClassPractiseTest extends BaseTest {
+//
+//    @Test
+//    public void testRobotFunctionality() throws AWTException {
+//        PasswordCheck passwordCheck = new PasswordCheck(driver);
+//        passwordCheck.openURL("https://ccnp-app.knocktest.com/");
+//        passwordCheck.login();
+//        passwordCheck.googlePasswordManager();
+//    }
+//}
